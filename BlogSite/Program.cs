dbd7f843f0819ad9ab0ed4b2f5072ec3123b3345
Elanur-20 <elanurbuyukgoz@gmail.com>
2025-07-08 15:51:10 +0300

    test

--- BlogSite/Program.cs
+++ BlogSite/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+ 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
